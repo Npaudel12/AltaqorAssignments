b8fe7ff6b515803a1fa28840c932e28627638df5
Npaudel12 <narayan.paudel84@gmail.com>
2023-05-02 12:16:40 -0500

    07 assignment

---
+++ src/altaqor/assignments/assignment07/Car.java
@@ -0,0 +1,23 @@
+package altaqor.assignments.assignment07;
+
+public class Car {
+
+	public static void main(String[] args) {
+	
+		// local variable declaration
+		String make = "Subaru";
+		String model = "Forestor";
+		int year = 2017;
+		String color = "Red";
+		
+		// printing local variable
+		
+		System.out.println("The make of my Car:  "  + make);
+		System.out.println("The model of my Car: "  + model);
+		System.out.println("The year of my Car:  "  +  year);
+		System.out.println("The color of my Car: "  + color);
+		
+
+	}
+
+}
