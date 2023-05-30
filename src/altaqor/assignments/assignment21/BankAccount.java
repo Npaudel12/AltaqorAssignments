a683653006348ff692184d039168bee234bddd7a
Npaudel12 <narayan.paudel84@gmail.com>
2023-05-29 22:46:56 -0500

    1st commit

--- src/altaqor/assignments/assignment21/BankAccount.java
+++ src/altaqor/assignments/assignment21/BankAccount.java
@@ -1,11 +1,6 @@
 package altaqor.assignments.assignment21;
 
 public interface BankAccount {
-	public  double balance =0;
-	public double deposit = 0;
-	public double withdraw = 0;
-	public double intrest =0;
-	
 	
 	public void deposit(double amount);
 	public void withdraw(double amount);
