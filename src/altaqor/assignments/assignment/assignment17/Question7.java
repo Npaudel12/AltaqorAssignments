e2a965913f36fe00175b56a06090dcbbff418d43
Npaudel12 <narayan.paudel84@gmail.com>
2023-06-11 00:36:44 -0500

    Assignment26

--- src/altaqor/assignments/assignment/assignment17/Question7.java
+++
@@ -1,9 +0,0 @@
-package altaqor.assignment.assignment17;
-//Write a Java program that takes a sentence as input and capitalizes the first letter of each word
-public class Question7 {
-
-	public static void main(String[] args) {
-		
-		String sentence = " my name is binam paudel";
-	}
-}
---
+++ src/altaqor/assignments/selenium/assignment26/SeleniumIDPractice.java
@@ -0,0 +1,44 @@
+package altaqor.assignments.selenium.assignment26;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class SeleniumIDPractice {
+
+	public static void main(String[] args) {
+		// property setup
+		System.setProperty("webdriver.chrome.driver","C:\\Driver\\chromedriver_win32\\chromedriver.exe");
+		
+		// ChromeOptions
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("start-maximized");
+		options.addArguments("remote-allow-origins=*");
+		
+		// instance of ChromeDriver
+		WebDriver driver = new ChromeDriver(options);
+		
+		// URL LUNCH
+		driver.get("https://ecommerce-playground.lambdatest.io/index.php?route=account/register");
+		
+		// sending values in text fields
+		driver.findElement(By.id("input-firstname")).sendKeys("Binam");
+		driver.findElement(By.id("input-lastname")).sendKeys("Paudel");
+		driver.findElement(By.id("input-email")).sendKeys("Binam@mmm.com");
+		driver.findElement(By.id("input-telephone")).sendKeys("40234021116");
+		driver.findElement(By.id("input-password")).sendKeys("Paudel12");
+		driver.findElement(By.id("input-confirm")).sendKeys("Paudel12");
+		
+		// close
+		try {
+			Thread.sleep(5000);
+			driver.close();
+		}catch(Exception e ) {
+			
+		}
+		
+
+	}
+
+}
